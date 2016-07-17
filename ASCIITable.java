ef15bf62c75ae9332563dbac54c567cd9edd4b99
Mega Man <register@wissen1000.de>
2016-07-17 11:29:28 +0200

    ASCIITable.java should be ready

---
+++ ASCIITable.java
@@ -0,0 +1,10 @@
+public class ASCIITable {
+;
+public Hashmap<Integer, Character> ascii = new HashMap<Integer, Character>();
+
+public void init(){
+for(i=0;i<255;i++){
+ascii.put(i,(char)i)
+}
+}
+}
